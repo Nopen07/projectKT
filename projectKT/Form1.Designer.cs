77e40ff63db52fd111ee623ad8b6be18ef71c463
nopen07 <tailatao2580@gmail.com>
2023-03-01 09:15:55 +0700

    them label mat khau, button txtmatkhau

--- projectKT/Form1.Designer.cs
+++ projectKT/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.textUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mat khau";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(266, 149);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(207, 22);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // textUserName
+            // 
+            this.textUserName.Location = new System.Drawing.Point(276, 103);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(207, 22);
+            this.textUserName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "user name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox textUserName;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- projectKT/Form1.cs
+++ projectKT/Form1.cs
@@ -16,5 +16,10 @@ namespace projectKT
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
