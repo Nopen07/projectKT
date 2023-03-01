a274d5925f0216a192aa2d75199a0786396cd428
nopen07 <tailatao2580@gmail.com>
2023-03-01 09:25:28 +0700

    sua label ten dang nhap, mat khau

--- projectKT/Form1.Designer.cs
+++ projectKT/Form1.Designer.cs
@@ -29,52 +29,63 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
             this.textUserName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(73, 155);
+            this.label1.Location = new System.Drawing.Point(70, 155);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mat khau";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // txtMatKhau
+            // txtPassWord
             // 
-            this.txtMatKhau.Location = new System.Drawing.Point(266, 149);
-            this.txtMatKhau.Name = "txtMatKhau";
-            this.txtMatKhau.Size = new System.Drawing.Size(207, 22);
-            this.txtMatKhau.TabIndex = 1;
+            this.txtPassWord.Location = new System.Drawing.Point(266, 149);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(217, 22);
+            this.txtPassWord.TabIndex = 1;
             // 
             // textUserName
             // 
-            this.textUserName.Location = new System.Drawing.Point(276, 103);
+            this.textUserName.Location = new System.Drawing.Point(266, 103);
             this.textUserName.Name = "textUserName";
-            this.textUserName.Size = new System.Drawing.Size(207, 22);
+            this.textUserName.Size = new System.Drawing.Size(217, 22);
             this.textUserName.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 109);
+            this.label2.Location = new System.Drawing.Point(70, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.Size = new System.Drawing.Size(103, 16);
             this.label2.TabIndex = 2;
-            this.label2.Text = "user name";
+            this.label2.Text = "Ten Dang Nhap";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(301, 238);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 47);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textUserName);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtPassWord);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -86,9 +97,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtPassWord;
         private System.Windows.Forms.TextBox textUserName;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
